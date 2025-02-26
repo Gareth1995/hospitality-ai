c8f174ced766e969426d1f182b87d9281236a9f8
Gareth <gareth.reeve50@gmail.com>
2025-02-26 15:32:32 +0200

    changed naming for rating by room plot. Naming still not correct

--- app/components/room_type_sentiment.tsx
+++ app/components/room_type_sentiment.tsx
@@ -14,25 +14,6 @@ import {
 } from "recharts";
 import { useAuth } from "../context/authContext";
 
-// Define TypeScript type for data
-// interface ChartData {
-//     name: string;
-//     uv: number;
-//     pv: number;
-//     amt: number;
-// }
-
-// Sample data
-// const data: ChartData[] = [
-//     { name: "Page A", uv: 4000, pv: 2400, amt: 2400 },
-//     { name: "Page B", uv: 3000, pv: 1398, amt: 2210 },
-//     { name: "Page C", uv: 2000, pv: 9800, amt: 2290 },
-//     { name: "Page D", uv: 2780, pv: 3908, amt: 2000 },
-//     { name: "Page E", uv: 1890, pv: 4800, amt: 2181 },
-//     { name: "Page F", uv: 2390, pv: 3800, amt: 2500 },
-//     { name: "Page G", uv: 3490, pv: 4300, amt: 2100 }
-// ];
-
 const RoomTypeSentiment: React.FC = () => {
     const { hotelId } = useAuth();
     const [ratingByRoom, setRatingByRoom] = useState([]);
@@ -71,16 +52,32 @@ const RoomTypeSentiment: React.FC = () => {
         <ResponsiveContainer width="100%" height={400}>
             <BarChart
                 data={ratingByRoom}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                margin={{ top: 20, right: 30, left: 40, bottom: 60 }}
             >
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
+                <XAxis 
+                    dataKey="name" 
+                    tick={({ x, y, payload }) => {
+                        const text = payload.value;
+                        const chunkSize = 10;
+                        
+                        // Split text into chunks of 10 characters
+                        const chunks = text.match(new RegExp(`.{1,${chunkSize}}`, "g")) || [];
+
+                        return (
+                            <text x={x} y={y} textAnchor="middle" fontSize={15}>
+                                {chunks.map((chunk, index) => (
+                                    <tspan x={x} dy={15} key={index}>{chunk}</tspan>
+                                ))}
+                            </text>
+                        );
+                    }} 
+                />
+
                 <YAxis />
                 <Tooltip />
                 <Legend />
                 <Bar dataKey="average_rating" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-                {/* <Bar dataKey="average_rating" stackId="a" fill="#8884d8" /> */}
-                {/* <Bar dataKey="uv" stackId="a" fill="#82ca9d" /> */}
             </BarChart>
         </ResponsiveContainer>
     );
